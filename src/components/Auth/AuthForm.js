6762f29a1ffedce9179671f653ed4e654d61c589
YevaMV <eva280988@gmail.com>
2022-04-24 23:10:24 +0400

    added input validation

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,9 +1,36 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(true);
+  const [enteredPasswordIsValid, setEnteredPasswordIsValid] = useState(true);
+  const enteredInputRef = useRef();
+  const enteredinputPassword = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = enteredInputRef.current.value;
+    const enteredPassword = enteredinputPassword.current.value;
+
+    const enteredEmailHasError = enteredEmail.trim() === '';
+    const enteredPasswordHasError = enteredPassword.trim().length !== 6;
+
+    if (enteredEmailHasError) {
+      setEnteredEmailIsValid(false);
+    }
+
+    if (enteredPasswordHasError) {
+      setEnteredPasswordIsValid(false);
+    }
+
+    if (enteredEmailHasError && enteredPasswordHasError) {
+      setEnteredEmailIsValid(false);
+      setEnteredPasswordIsValid(false);
+    }
+  };
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -12,19 +39,27 @@ const AuthForm = () => {
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={enteredInputRef} />
+          {!enteredEmailIsValid && (
+            <p className="error-text">Email can not be empty</p>
+          )}
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" ref={enteredinputPassword} />
+          {!enteredPasswordIsValid && (
+            <p className="error-text">
+              Please enter a valid Password(6 charachters long)!
+            </p>
+          )}
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
